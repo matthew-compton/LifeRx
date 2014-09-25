6870143685d3b1c3976312d430cdcf86ec02e74b
matthew-compton <compton@bignerdranch.com>
2014-09-25 16:17:03 -0400

    Added move counter functionality

--- app/src/main/java/com/bignerdranch/android/liferx/LifeFragment.java
+++ app/src/main/java/com/bignerdranch/android/liferx/LifeFragment.java
@@ -1,12 +1,14 @@
 package com.bignerdranch.android.liferx;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -31,12 +33,16 @@ public class LifeFragment extends Fragment {
     @InjectView(R.id.button7) ToggleButton tileToggleButton7;
     @InjectView(R.id.button8) ToggleButton tileToggleButton8;
     @InjectView(R.id.button9) ToggleButton tileToggleButton9;
-    @InjectView(R.id.button_restart) Button buttonRestart;
+
+    @InjectView(R.id.restart) Button buttonRestart;
+    @InjectView(R.id.timer) TextView mTimerTextView;
+    @InjectView(R.id.counter) TextView mCounterTextView;
 
     private List<ToggleButton> tileButtons;
+    private boolean mIsStarted;
 
-    Subscription mSubscription;
-    PublishSubject<ToggleButton> mTileClickSubject;
+    private Subscription mSubscription;
+    private PublishSubject<ToggleButton> mToggleButtonPublishSubject;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -55,22 +61,94 @@ public class LifeFragment extends Fragment {
             add(tileToggleButton9);
         }};
 
-        mTileClickSubject = PublishSubject.create();
-        mSubscription = mTileClickSubject.asObservable().subscribe(new Action1<ToggleButton>() {
-            @Override
-            public void call(ToggleButton button) {
-                toggleAdjacent(button);
-                if (isVictory()) {
-                    disable();
-                    Toast.makeText(getActivity(), getString(R.string.victory), Toast.LENGTH_SHORT).show();
+        mIsStarted = false;
+
+        mToggleButtonPublishSubject = PublishSubject.create();
+        mSubscription = mToggleButtonPublishSubject.asObservable().subscribe(
+                new Action1<ToggleButton>() {
+                    @Override
+                    public void call(ToggleButton button) {
+                        incrementCounter();
+                        if (!mIsStarted) {
+                            mIsStarted = true;
+                            mCountDownTimer.start();
+                        }
+                        toggleAdjacentToggleButtons(button);
+                        if (checkForVictory()) {
+                            isVictory();
+                        }
+                    }
                 }
-            }
-        });
+        );
 
         return layout;
     }
 
-    private void toggleAdjacent(ToggleButton toggleButton) {
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mSubscription != null) {
+            mSubscription.unsubscribe();
+        }
+    }
+
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9})
+    public void activate(ToggleButton toggleButton) {
+        mToggleButtonPublishSubject.onNext(toggleButton);
+    }
+
+    @OnClick(R.id.restart)
+    public void restart() {
+        mCountDownTimer.cancel();
+        mIsStarted = false;
+        mCounterTextView.setText(getString(R.string.zero));
+        mTimerTextView.setText(getString(R.string.time_max));
+        mTimerTextView.setTextColor(getResources().getColor(android.R.color.white));
+        resetToggleButtons();
+    }
+
+    private boolean checkForVictory() {
+        for (ToggleButton toggleButton : tileButtons) {
+            if (!toggleButton.isChecked()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void isVictory() {
+        mCountDownTimer.cancel();
+        disableToggleButtons();
+        mTimerTextView.setTextColor(getResources().getColor(android.R.color.holo_green_light));
+        Toast.makeText(getActivity(), getString(R.string.victory), Toast.LENGTH_SHORT).show();
+    }
+
+    private void isDefeat() {
+        disableToggleButtons();
+        mTimerTextView.setText(getString(R.string.zero));
+        mTimerTextView.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+        Toast.makeText(getActivity(), getString(R.string.defeat), Toast.LENGTH_SHORT).show();
+    }
+
+    private void disableToggleButtons() {
+        for (ToggleButton toggleButton : tileButtons) {
+            toggleButton.setEnabled(false);
+        }
+    }
+
+    private void resetToggleButtons() {
+        for (ToggleButton toggleButton : tileButtons) {
+            toggleButton.setEnabled(true);
+            toggleButton.setChecked(false);
+        }
+    }
+
+    private void incrementCounter() {
+        Integer counter = Integer.parseInt(mCounterTextView.getText().toString()) + 1;
+        mCounterTextView.setText(counter.toString());
+    }
+
+    private void toggleAdjacentToggleButtons(ToggleButton toggleButton) {
         switch (toggleButton.getId()) {
             case R.id.button1:
                 tileToggleButton2.toggle();
@@ -117,40 +195,17 @@ public class LifeFragment extends Fragment {
         }
     }
 
-    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9})
-    public void activate(ToggleButton toggleButton) {
-        mTileClickSubject.onNext(toggleButton);
-    }
+    private CountDownTimer mCountDownTimer = new CountDownTimer(10000, 1000) {
 
-    @OnClick(R.id.button_restart)
-    public void restart() {
-        for (ToggleButton toggleButton : tileButtons) {
-            toggleButton.setEnabled(true);
-            toggleButton.setChecked(false);
+        public void onTick(long millisUntilFinished) {
+            String seconds = ((Long) (millisUntilFinished / 1000)).toString();
+            mTimerTextView.setText(seconds);
         }
-    }
 
-    private boolean isVictory() {
-        for (ToggleButton toggleButton : tileButtons) {
-            if (!toggleButton.isChecked()) {
-                return false;
-            }
+        public void onFinish() {
+            isDefeat();
         }
-        return true;
-    }
 
-    private void disable() {
-        for (ToggleButton toggleButton : tileButtons) {
-            toggleButton.setEnabled(false);
-        }
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (mSubscription != null) {
-            mSubscription.unsubscribe();
-        }
-    }
+    };
 
 }
